21ba9a3d64229755959cc0114f6d83d1001b88c0
Mr_Unknown19 <khaled.taha1918@gmail.com>
2023-12-04 01:41:09 +0200

    getting ready

---
+++ Inventory.h
@@ -0,0 +1,11 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Inventory.h"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
