38354eefd2047b58143c9b5209bca3b52c76d089
Mr_Unknown9 <khaled.taha1918@gmail.com>
2023-12-04 03:10:41 +0200

    added the print/isEmpty functions

--- Inventory.h
+++ Inventory.h
@@ -6,6 +6,41 @@
 
 using namespace std;
 
+struct Node
+{
+    string info;
+    Node *next;
+};
 
+class Inventory
+{
+private:
+    Node *head;
+public:
+    Inventory()
+    {
+        head = NULL;
+    };
 
+    bool isEmpty()
+    {
+        return (head == NULL);
+    }
+
+    void print()
+    {
+        if (!isEmpty())
+        {
+            Node *temp = head;
+            while (temp->next != NULL)
+            {
+                cout << temp->info << "  ";
+                temp->next;
+            }
+        }
+        else
+            cout << "The Inventory Is Empty.";
+    }
+
+};
 #endif
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "Inventory.h"
+// INCLUDE the header file, i don't want to kill you 😇 
 
 using namespace std;
 
 int main()
 {
+    Inventory I;
 
     return 0;
 }
