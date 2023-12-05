c2f97d3d39ee0eed4e40197689c68af491e49331
Mr_Unknown9 <khaled.taha1918@gmail.com>
2023-12-05 02:42:04 +0200

    Note

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// This is a hardware repo to store the parts
+// and track the parts and₫the spare
+
 #include <iostream>
 #include "Inventory.h"
 // INCLUDE the header file, i don't want to kill you 😇 
