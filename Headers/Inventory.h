18d956a364cbd4db140f59f53d3555888175213c
dark0danger <77031940+dark0danger@users.noreply.github.com>
2023-12-07 19:46:27 +0200

    Update Inventory.h

--- Headers/Inventory.h
+++ Headers/Inventory.h
@@ -42,40 +42,5 @@ void printMenu() {
     std::cout << "Enter your choice: ";
 }
 
-int main() {
-    Inventory inventory;
-    int choice;
-    std::string type, name;
-
-    while (true) {
-        printMenu();
-        std::cin >> choice;
-
-        switch (choice) {
-            case 1:
-                std::cout << "Enter component type: ";
-                std::cin >> type;
-                std::cout << "Enter component name: ";
-                std::cin >> name;
-                // inventory.addComponent(type, name);
-                break;
-            case 2:
-                std::cout << "Enter component name to remove: ";
-                std::cin >> name;
-                // inventory.removeComponent(name);
-                break;
-            case 3:
-                std::cout << "\nCurrent Inventory:\n";
-                // inventory.displayInventory();
-                break;
-            case 4:
-                return 0;
-            default:
-                std::cout << "Invalid choice\n";
-        }
-    }
-
-    return 0;
-}
 
 #endif
